d79f4197f71f7d6066c87717a3d9f089d4ad09db
GitHub Action <action@github.com>
2025-05-24 01:49:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/kamadakohei/terraformstylechecker/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kamadakohei/terraformstylechecker/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.kamadakohei.terraformstylechecker.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kamadakohei.terraformstylechecker.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
